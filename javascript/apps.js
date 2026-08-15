f8fe0fc445d022e3e64109ee6c1053bcc71098e0
Sania Charpe <146666606+iambasicallysania@users.noreply.github.com>
2026-08-15 21:46:06 +0530

    Add files via upload

---
+++ javascript/apps.js
@@ -0,0 +1,20 @@
+// console.log("hello world!");
+
+// let light = "green";
+// if(light =="red"){
+//     console.log("Stop");
+// }
+// if(light =="yellow"){
+//     console.log("Get ready");
+// }
+// if(light =="green"){
+//     console.log("Go");
+// }
+
+let str = "hello world!";
+
+if(str.length > 3 && str[0] == "a"){
+    console.log("good string");
+}else{
+    console.log("bad string");
+}
